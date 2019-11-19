2da892c46b7c38cece15cac9dd265dcb74963d40
Balaji Venkatesan <balaji.v@ezetap.com>
2019-11-19 12:06:38 +0530

    added

--- src/main/java/com/practise/demo/paymentdemo/cache/RedisRepositoryImplementation.java
+++ src/main/java/com/practise/demo/paymentdemo/cache/RedisRepositoryImplementation.java
@@ -2,6 +2,7 @@ package com.practise.demo.paymentdemo.cache;
 
 import com.practise.demo.paymentdemo.model.Account;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
@@ -10,7 +11,7 @@ import org.springframework.stereotype.Repository;
 import javax.annotation.PostConstruct;
 
 @Repository
-public class RedisRepositoryImplementation implements RedisRepository {
+public class RedisRepositoryImplementation implements RedisRepository  {
 
     private RedisTemplate<String,String> redisTemplate;
     private ValueOperations valueOperations;
